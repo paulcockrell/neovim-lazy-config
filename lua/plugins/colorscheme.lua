2c7e297f7763e4c4b3ae56a737ec89f3b83528d8
Paul Cockrell <paulcockrell@gmail.com>
2024-08-23 14:24:27 +0100

    Update color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -10,15 +10,25 @@
 --     end,
 --   },
 -- }
+
+-- return {
+--   {
+--     "rose-pine/neovim",
+--     name = "rose-pine",
+--     priority = 1000,
+--     config = function()
+--       -- vim.cmd("colorscheme rose-pine-main")
+--       vim.cmd("colorscheme rose-pine-moon")
+--       --vim.cmd.colorscheme("rose-pine-dawn")
+--     end,
+--   },
+-- }
+
 return {
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    priority = 1000,
-    config = function()
-      -- vim.cmd("colorscheme rose-pine-main")
-      --vim.cmd("colorscheme rose-pine-moon")
-      vim.cmd.colorscheme("rose-pine-dawn")
-    end,
-  },
+  "xero/miasma.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd("colorscheme miasma")
+  end,
 }
